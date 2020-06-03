d1ff7e14ae076307510c950c08ea4393d7b8358e
Chris O'leary <chris_oleary@hotmail.co.uk>
2020-06-03 12:49:38 +0100

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RPG
+{
+    // make it available across the game
+    enum Dir
+    {
+        Down, 
+        Up, 
+        Left, 
+        Right
+    }
+    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // player sprites
+        Texture2D player_Sprite;
+        Texture2D playerDown;
+        Texture2D playerUp;
+        Texture2D playerLeft;
+        Texture2D playerRight;
+        
+        // enemy sprites
+        Texture2D eyeEnemy_Sprite;
+        Texture2D snakeEnemy_Sprite;
+
+        // obstacle sprites
+        Texture2D bush_Sprite;
+        Texture2D tree_Sprite;
+
+        // misc sprites
+        Texture2D heart_Sprite;
+        Texture2D bullet_Sprite;
+
+        Player player = new Player();
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // game window size
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+        }
+
+       
+        protected override void Initialize()
+        {
+           
+
+            base.Initialize();
+        }
+
+       
+        protected override void LoadContent()
+        {
+          
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // player
+            player_Sprite = Content.Load<Texture2D>("Player/player");
+            playerDown = Content.Load<Texture2D>("Player/playerDown");
+            playerUp = Content.Load<Texture2D>("Player/playerUp");
+            playerLeft = Content.Load<Texture2D>("Player/playerLeft");
+            playerRight = Content.Load<Texture2D>("Player/playerRight");
+
+            // enemies
+            eyeEnemy_Sprite = Content.Load<Texture2D>("Enemies/eyeEnemy");
+            snakeEnemy_Sprite = Content.Load<Texture2D>("Enemies/snakeEnemy");
+
+            // obstacles
+            bush_Sprite = Content.Load<Texture2D>("Obstacles/bush");
+            tree_Sprite = Content.Load<Texture2D>("Obstacles/tree");
+
+            // misc
+            heart_Sprite = Content.Load<Texture2D>("Misc/heart");
+            bullet_Sprite = Content.Load<Texture2D>("Misc/bullet");
+
+           
+        }
+
+      
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        
+      
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+            
+            // player movement
+            player.Update(gameTime);
+        }
+
+       
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.ForestGreen);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(player_Sprite, player.Position, Color.White);
+
+            spriteBatch.End();
+          
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    class Player
+    {
+        private Vector2 position = new Vector2(100, 100);
+        private int speed = 200;
+        private Dir direction = Dir.Down;
+        private bool isMoving = false;
+
+        // this may need changing
+        public int Health { get; set; } = 3;
+
+        // using a set for a Vector2 is tricker and is done below 
+        public Vector2 Position {
+            get
+            {
+                return position;
+            }
+        }
+        public void setX(float newX)
+        {
+            position.X = newX;
+        }
+        public void setY(float newY)
+        {
+            position.Y = newY;
+        }
+
+        // player movement
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState kstate = Keyboard.GetState();
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // this means every frame the player stops moving
+            isMoving = false;
+            // however if the keys are held, player will move
+            if (kstate.IsKeyDown(Keys.Right))
+            {
+                direction = Dir.Right;
+                isMoving = true;
+            }
+            if (kstate.IsKeyDown(Keys.Left))
+            {
+                direction = Dir.Left;
+                isMoving = true;
+            }
+            if (kstate.IsKeyDown(Keys.Up))
+            {
+                direction = Dir.Up;
+                isMoving = true;
+            }
+            if (kstate.IsKeyDown(Keys.Down))
+            {
+                direction = Dir.Down;
+                isMoving = true;
+            }
+           
+
+            if (isMoving)
+            {
+                switch (direction)
+                {
+                    case Dir.Down:
+                        position.Y += speed * dt;
+                        break;
+                    case Dir.Up:
+                        position.Y -= speed * dt;
+                        break;
+                    case Dir.Left:
+                        position.X -= speed * dt;
+                        break;
+                    case Dir.Right:
+                        position.X += speed * dt;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
